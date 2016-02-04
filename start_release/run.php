496f4eb1fb8d0606cb2810b0f26f87f9ae83b7b1
Mat Evans <mat.evans@valtech.co.uk>
2016-02-04 16:08:16 +0000

    Change some repos to DevRepos

--- start_release/run.php
+++ start_release/run.php
@@ -16,20 +16,21 @@ $repoTemplate = 'git@gitlab.inf.mgt.mtpdvsa:olcs/%s.git';
 
 $repos = [
     ModuleRepo::create('olcs-common', $repoTemplate),
-    ModuleRepo::create('olcs-static', $repoTemplate),
     ModuleRepo::create('olcs-transfer', $repoTemplate),
     ModuleRepo::create('olcs-utils', $repoTemplate),
-    ModuleRepo::create('olcs-config', $repoTemplate),
-    ModuleRepo::create('olcs-elasticsearch', $repoTemplate),
     ModuleRepo::create('olcs-auth', $repoTemplate),
-    ModuleRepo::create('olcs-oa', $repoTemplate),
+
+    ModuleRepo::create('olcs-config', $repoTemplate),
 
     AppRepo::create('olcs-selfserve', $repoTemplate),
     AppRepo::create('olcs-internal', $repoTemplate),
     AppRepo::create('olcs-backend', $repoTemplate),
     AppRepo::create('olcs-scanning', $repoTemplate),
 
-    DevRepo::create('olcs-etl', $repoTemplate)
+    DevRepo::create('olcs-etl', $repoTemplate),
+    DevRepo::create('olcs-elasticsearch', $repoTemplate),
+    DevRepo::create('olcs-oa', $repoTemplate),
+    DevRepo::create('olcs-static', $repoTemplate),
 ];
 
 // Validate all repos before processing
