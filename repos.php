6bcb58e024c1c464beab655d7813c1839e22ffc4
Mat Evans <mat.evans@valtech.co.uk>
2016-02-16 17:07:44 +0000

    Added some comments to repo list

--- repos.php
+++ repos.php
@@ -5,14 +5,18 @@ include(__DIR__ . '/lib.php');
 $repoTemplate = 'git@gitlab.inf.mgt.mtpdvsa:olcs/%s.git';
 
 $repos = [
+    // Do these dependencies need adding or could composer.json point at their master
+//    ModuleRepo::create('autoload', $repoTemplate),
+//    ModuleRepo::create('companies-house', $repoTemplate),
+//    ModuleRepo::create('olcs-xmltools', $repoTemplate),
+//    ModuleRepo::create('olcs-logging', $repoTemplate),
+
     ModuleRepo::create('olcs-config', $repoTemplate),
 
     ModuleRepo::create('olcs-common', $repoTemplate),
     ModuleRepo::create('olcs-transfer', $repoTemplate),
     ModuleRepo::create('olcs-utils', $repoTemplate),
     ModuleRepo::create('olcs-auth', $repoTemplate),
-    //ModuleRepo::create('olcs-logging', $repoTemplate),
-
 
     AppRepo::create('olcs-selfserve', $repoTemplate),
     AppRepo::create('olcs-internal', $repoTemplate),
@@ -21,6 +25,8 @@ $repos = [
 
     DevRepo::create('olcs-etl', $repoTemplate),
     DevRepo::create('olcs-elasticsearch', $repoTemplate),
-    DevRepo::create('olcs-oa', $repoTemplate),
     DevRepo::create('olcs-static', $repoTemplate),
+
+    // OA repo would need updating before packaging, ie checking ERB files, therefore should be tagged then?
+    DevRepo::create('olcs-oa', $repoTemplate),
 ];
