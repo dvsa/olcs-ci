0397fce48ae6ef29f08eb53aac7faca15a51c35b
Mat Evans <mat.evans@valtech.co.uk>
2016-10-20 11:09:27 +0100

    Added update dependencies script

---
+++ shell/update-olcs-dependencies.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# tag to create, a release this will expect to find a release branch named "release/X" X = tag
+branch=$1
+
+if [ -e $branch ]; then
+  echo "Branch not sepcified, Eg 'release/4.0.2'"
+  exit
+fi
+
+source config.sh
+
+# Override repos as we only need these three
+repos=(
+  "olcs-backend"
+  "olcs-internal"
+  "olcs-selfserve"
+)
+
+echo "Branch : $branch"
+echo
+
+cloneAll
+
+cd $reposDir
+startPath=`pwd`
+for dir in "${repos[@]}"; do
+  echo
+  echo "== $dir =="
+  echo
+
+  cd $startPath/$dir
+
+  git checkout -q $branch || exit
+
+  composer update olcs/*
+
+  git add composer.lock
+
+  git commit -m'Update olcs/* dependencies'
+
+  git push
+done
