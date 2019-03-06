02b5d50cae141f97f39cf74869333984d78f4bd2
James Hodgson <james@tdchodgy.co.uk>
2019-03-06 15:51:40 +0000

    Changes to clean tag script

--- shell/clean-tags.sh
+++ shell/clean-tags.sh
@@ -1,37 +1,37 @@
-```#!/usr/bin/env bash
+#!/usr/bin/env bash
 
 pwd=`pwd`
 report=$pwd/tag-cleanup.txt
-cd
+
 echo "Tag Clean Up Report" > $report
 echo "-------------------" >> $report
 echo "" >> $report
 
-for project in */ ; do
+for project in $pwd/*/ ; do
     echo "+--------------------+" >> $report
     echo "|Processing $project " >> $report
     echo "+--------------------+" >> $report
-    cd $pwd/$project
+    cd $project
+    pwd
     git fetch --tags 2>/dev/null
     count=0
     echo "<Tag History>" > TAGS
-    for tag in $(git for-each-ref refs/tags --sort=-taggerdate --format='%(refname)') ; do
+    for tag in $(git for-each-ref refs/tags --sort=-taggerdate --format='%(refname:short)') ; do
         hash=`git rev-parse $tag`
         echo "${hash} -> ${tag}" >> TAGS
-        count=$((count+1))
-        if (( $count > 3 )); then
-            # git push --delete origin $tag
-            # git tag -d $tag
+        if (( $count > 2 )); then
+            git push --delete origin refs/tags/$tag
+            `git tag -d $tag`;
             echo "DELETED: $tag for $hash" >> $report
         else
             echo "KEPT:    $tag for $hash" >> $report
         fi
+        count=$((count+1))
     done
-    # git checkout develop 2>/dev/null
-    # git branch -b tag-cleanup 2>/dev/null
-    # git add TAGS
-    # git commit -m '[AUTO] Added file to track tags for cleanup'
-    # git push --set-upstream origin feature/tag-cleanup
+    git checkout develop 2>/dev/null
+    git checkout -b feauture/tag-cleanup 2>/dev/null 
+    git add TAGS
+    git commit -m '[AUTO] Added file to track tags following cleanup'
+    git push --set-upstream origin feauture/tag-cleanup
     echo "" >> $report
-done```
-
+done
